2756e0cc2266311f3a6e72c7da67d76a8dacee9d
JenishaB2712 <148978245+JenishaB2712@users.noreply.github.com>
2024-01-02 18:21:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,20 +6,14 @@ l = []
 @app.route('/')
 def index():
     return render_template('index.html')
+    
 @app.route('/')
 @app.route('/total_combinations')
-def get_total_combinations():
+def get_total_combinations_and_get_distribution_list_and_get_probability():
     number_of_dice = 2
     sides = 6
     total_combinations = sides ** number_of_dice
-    return jsonify(total_combinations)
     
-
-
-@app.route('/')
-@app.route('/distribution')
-def get_distribution_list():
-    sides = 6
     distribution_list = []
     for i in range(1, sides + 1):
         for j in range(1, sides + 1):
@@ -31,21 +25,7 @@ def get_distribution_list():
             }
             l.append(total)
             distribution_list.append(combination_map)
-    return jsonify(distribution_list)
      
-@app.route('/')
-@app.route('/probability')
-def get_probability():
-    sides =6
-    for i in range(1, sides + 1):
-        for j in range(1, sides + 1):
-            total = i + j
-            combination_map = {
-                "Die A": i,
-                "Die B": j,
-                "Sum": total,
-            }
-            l.append(total)
 
     probability = []
     x = list(set(l))
@@ -53,11 +33,11 @@ def get_probability():
         probability.append(l.count(x[i]))
         probability[i] = f"'{probability[i]}':'{str(l.count(x[i]))}/{36}'"
 
-    #output1 = {"total_combinations": total_combinations}
-    #output2 = {"distribution": distribution_list}
+    output1 = {"total_combinations": total_combinations}
+    output2 = {"distribution": distribution_list}
     output3 = {"probability": probability}
 
-    return jsonify(output3)
+    return jsonify(output1,output2,output3)
 
 if __name__ == "__main__":
     app.run(debug=True)
